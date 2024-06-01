36795821fa94294ffd30cf6f94266d141771aa5b
Vijay-948 <kvijayreddy948@gmail.com>
2024-06-01 16:45:34 +0530

    code-refactoring

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ProjectData from './ProjectData';
 import { Tilt } from 'react-tilt';
+import { Scale } from '@mui/icons-material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+
 
 const ProjectCard = () => {
     return (
@@ -12,13 +16,32 @@ const ProjectCard = () => {
                     Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
                 </p>
             </div>
-            <div className="grid grid-cols-1 md: grid-cols-2 lg:grid-cols-2 gap-8 mt-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-8">
                 {ProjectData.map((project, index) => (
-                    <Tilt>
-                        <div className="bg-gray-100">
+                    <Tilt options={{
+                        max: 45,
+                        scale: 1,
+                        speed: 450,
+                      }}
+                      className="bg-tertiary p-5 rounded-2xl sm:w-[470px] lg:w-[470px] w-full">
+                        <div className="bg-gray-100 rounded-xl shadow-xl p-6 Tilt-inner transform transistion duration-500 hover:scale-105">
                             <div className="flex flex-col items-center">
                                 <h4 className='text-xl font-semibold m-2'>{project.name}</h4>
-                                <img src={project.image} alt={`Project $(index + 1)`} className='w-11/12 h-70 object-cover rounded-2xl mb-20' />
+                                <img src={project.image} alt={`Project $(index + 1)`} className='w-11/12 h-70 object-cover rounded-2xl mb-4 shadow-2xl' />
+                                <p className='text-gray-400 mb-1 p-2'>{project.description}</p>
+                                <div className="flex justify-start mb-4 text-start">
+                                    {project.technologies.map((techStack, indx) => (
+                                        <span key={indx} className={`text${techStack.color}-600 mx-1`}>#{techStack.name}</span>
+                                    ))}
+                                </div>
+                                <div className='flex flex-row gap-8'>
+                                    <a className='bg-blue-900 text-white px-2 py-1 rounded-3xl inline-block mb-4 text-decoration no-underline' href={project.soruce_code_link}>
+                                        Code <GitHubIcon sx={{width: '20px', height:'20px', color: 'white'}}/>
+                                    </a>
+                                    <a className='bg-blue-900 text-white px-2 py-1 rounded-3xl inline-block mb-4 no-underline' href={project.soruce_code_link}>
+                                        Demo <OpenInNewIcon sx={{width: '20px', height:'30px', color: 'white', textDecoration: 'none'}}/>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </Tilt>
@@ -31,102 +54,3 @@ const ProjectCard = () => {
 }
 
 export default ProjectCard;
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import ProjectData from './ProjectData';
-// import Tilt from 'react-tilt';
-
-// const Projects = () => {
-//     return (
-//         <section id="projects" className="relative py-12 px-4 lg:px-16">
-//             <div className="text-center mb-12">
-//                 <p className="text-xl font-semibold">Browse My Recent</p>
-//                 <h1 className="text-4xl font-bold">Projects</h1>
-//             </div>
-//             <div className="flex flex-wrap justify-center -mx-4">
-//                 {ProjectData.slice(0, 2).map((project, index) => (
-//                     <div key={index} className="w-full md:w-1/2 lg:w-1/2 p-4">
-//                         <Tilt className="Tilt" options={{ max: 25, scale: 1.05 }}>
-//                             <div className="bg-gray-100 rounded-lg shadow-md p-6 Tilt-inner transform transition duration-500 hover:scale-105">
-//                                 <div className="flex flex-col items-center">
-//                                     <h4 className="text-xl font-semibold mb-2">{project.name}</h4>
-//                                     <img src={project.image} alt={`Project ${index + 1}`} className="w-11/12 h-40 object-cover rounded-2xl mb-4" />
-//                                     <p className="text-center text-gray-700 mb-4">{project.description}</p>
-//                                     <div className="flex justify-center mb-4">
-//                                         {project.technologies.map((tech, idx) => (
-//                                             <span key={idx} className={`text-${tech.color}-600 mx-1`}>{tech.name}</span>
-//                                         ))}
-//                                     </div>
-//                                     <button 
-//                                         className="bg-blue-500 text-white px-4 py-2 rounded-md"
-//                                         onClick={() => window.location.href = project.source_code_link}
-//                                     >
-//                                         GitHub
-//                                     </button>
-//                                 </div>
-//                             </div>
-//                         </Tilt>
-//                     </div>
-//                 ))}
-//             </div>
-//             <img src="./assets/arrow.png" alt="Arrow icon" className="w-12 h-12 mx-auto mt-8 cursor-pointer" onClick={() => window.location.href = '#contact'} />
-//         </section>
-//     );
-// };
-
-// export default Projects;
-
-
-
-
-
-/*
-const Projects = () => {
-    return (
-        <section id="projects" className="relative py-12">
-            <div className="text-center">
-                <p className="text-xl font-semibold">Browse My Recent</p>
-                <h1 className="text-4xl font-bold">Projects</h1>
-            </div>
-            <div className="flex flex-wrap justify-center mt-8">
-                {ProjectData.map((project, index) => (
-                    <div key={index} className="w-full md:w-1/2 lg:w-1/3 p-4">
-                        <div className="bg-gray-100 rounded-lg shadow-md p-6">
-                            <div className="flex flex-col items-center">
-                                <h4 className="text-xl font-semibold mb-2">{project.name}</h4>
-                                <img src={project.image} alt={`Project ${index + 1}`} className="w-19/20 h-60   object-cover rounded-2xl mb-4" />
-                                <p className="text-center text-gray-700 mb-4">{project.description}</p>
-                                <div className="flex justify-center mb-4">
-                                    {project.technologies.map((tech, idx) => (
-                                        <span key={idx} className={`text-${tech.color}-600 mx-1`}>#{tech.name}</span>
-                                    ))}
-                                </div>
-                                <button 
-                                    className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                                    onClick={() => window.location.href = project.source_code_link}
-                                >
-                                    GitHub
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </section>
-    );
-};
-
-export default Projects;
-
- */
--- src/Components/ProjectData.jsx
+++ src/Components/ProjectData.jsx
@@ -3,13 +3,11 @@ import img1 from '../Assests/ProjectCardImages/Car-Rental.jpg';
 const ProjectData = [
     {
         name: "Full Stack Project",
-        description: "hgdagkhahgkadg",
+        description: "Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.",
         technologies: [
             { name: "React", color: "blue" },
-            { name: "Typescript", color: "green" },
             { name: "Tailwindcss", color: "green" },
             { name: "Spring Boot", color: "green" },
-            { name: "Java", color: "blue" },
             {name: "MySQL", color: "blue"}
         ],
         image: img1,
@@ -18,13 +16,22 @@ const ProjectData = [
     },
     {
         name: "Frontend Project",
-        description: "hgdagkhahgkadg",
+        description: "Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.",
         technologies: [
             { name: "React", color: "blue" },
-            { name: "Typescript", color: "green" },
             { name: "Tailwindcss", color: "green" },
             { name: "Spring Boot", color: "green" },
-            { name: "Java", color: "blue" },
+            {name: "MySQL", color: "blue"}
+        ],
+        image: img1,
+        soruce_code_link: "https://github.com/Vijay-948/Car-Rental-UI",
+        live_website_link: ""
+    },
+    {
+        name: "Backend",
+        description: "Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.",
+        technologies: [
+            { name: "Spring Boot", color: "green" },
             {name: "MySQL", color: "blue"}
         ],
         image: img1,
@@ -33,26 +40,8 @@ const ProjectData = [
     },
     {
         name: "Backend",
-        description: "hgdagkhahgkadg",
+        description: "Following projects showcase my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.",
         technologies: [
-            { name: "React", color: "blue" },
-            { name: "Typescript", color: "green" },
-            { name: "Tailwindcss", color: "green" },
-            { name: "Spring Boot", color: "green" },
-            { name: "Java", color: "blue" },
-            {name: "MySQL", color: "blue"}
-        ],
-        image: img1,
-        soruce_code_link: "",
-        live_website_link: ""
-    },
-    {
-        name: "Backend",
-        description: "hgdagkhahgkadg",
-        technologies: [
-            { name: "React", color: "blue" },
-            { name: "Typescript", color: "green" },
-            { name: "Tailwindcss", color: "green" },
             { name: "Spring Boot", color: "green" },
             { name: "Java", color: "blue" },
             {name: "MySQL", color: "blue"}
