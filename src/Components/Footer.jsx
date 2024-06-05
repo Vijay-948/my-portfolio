dcc4f4a084645468d22391ec6f89a83bebbd7320
Vijay-948 <kvijayreddy948@gmail.com>
2024-06-05 21:38:27 +0530

    added-skills

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
         <>
-            <div className="mt-16 sm:mt-60 flex w-full py-8 mx-auto sm:flex-row text-center text-gray-50 bg-slate-900 items-center justify-center rounded-xl">
-                <p className="text-center font-semibold text-xl">
+            <div className="mt-5 flex w-full py-8 mx-auto sm:flex-row text-center bg-slate-900 items-center justify-center rounded-xl">
+                <p className="text-center font-semibold text-xl text-gray-400 hover:text-white">
                     &#128073; Copyright © 2024 Kogilathota Vijay Reddy. All Rights Reserved &#128515;.
                 </p>
             </div>
@@ -10,4 +10,4 @@ const Footer = () => {
     );
 }
 
-export default Footer;
+export default Footer;  
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -8,6 +8,7 @@ import css from '../Assests/css.png';
 import springboot from '../Assests/Spring boot.png';
 import sql from '../Assests/mysql.png';
 import git from '../Assests/git.png';
+import tailwind from '../Assests/tailwind.png';
   
 
 const Skills = () => {
@@ -21,7 +22,7 @@ const Skills = () => {
         { name: 'HTML', logo: html },
         { name: 'CSS', logo: css },
         { name: 'MySQL', logo: sql },
-        // {name: 'Tailwind', logo:aadf }
+        {name: 'TailwindCSS', logo:tailwind }
     ];
 
     return (
@@ -29,10 +30,10 @@ const Skills = () => {
         <section id="skills" className="py-10 px-5 md:px-20 min-h-screen flex flex-col items-center bg-gray-600">
             <p className="font-poppins font-bold text-xl text-white">Explore My</p>
             <h1 className="font-poppins font-medium text-4xl mt-2 mb-10 text-white">Skills</h1>
-            <div className="grid grid-cols-3 md:grid-cols-7 gap-10">
+            <div className="grid grid-cols-3 md:grid-cols-7 gap-16">
                 {languageandTools.map((item) => (
                     <div className="text-center">
-                        <img className= " rounded-full w-24 mx-auto mb-3 transform transition-transform hover:scale-105 hover:shadow-2xl hover:bg-gray-700" src={item.logo} alt={item.name} />
+                        <img className= "rounded-full w-24 mx-auto mb-2  transform transition-transform hover:scale-105 hover:shadow-2xl hover:bg-gray-700" src={item.logo} alt={item.name} />
                         <p className="font-poppins font-semibold text-white">{item.name}</p>
                     </div>
                 ))}
